654ee1f79348a7cec946127c841b804833e85adc
David Thomas <david@twostory.io>
2022-07-11 20:43:38 -0700

    Proper logging on deployment script

--- smartcontracts/scripts/deploy.js
+++ smartcontracts/scripts/deploy.js
@@ -7,16 +7,24 @@
 const hre = require("hardhat");
 
 async function main() {
+  console.log("[4] Getting Deployer's address")
+  const deployer = await hre.ethers.getSigner();
+  console.log("[+] Deploying contract with address: ", deployer.address)
+
+  console.log("[3] Getting the contract factory")
   const DAO = await hre.ethers.getContractFactory("DAO");
+
+  console.log("[2] Deploying contract")
   const dao = await DAO.deploy(
     "Decentralized Autonomous Organization",
     "DAO",
     "An example of a decentralized autonomous organization."
   );
 
+  console.log("[1] Awaiting deployment transaction to be mined")
   await dao.deployed();
 
-  console.log("DAO deployed to", dao.address);
+  console.log("[FINISHED] Contract deployed with address: ", dao.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
