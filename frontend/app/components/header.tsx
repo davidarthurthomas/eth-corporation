874efe478395c04c40ed6d9ffeeb93c71e988b03
David Thomas <david@twostory.io>
2022-07-13 09:39:51 -0700

    Init Remix

---
+++ frontend/app/components/header.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h1>Moralis Hello World!</h1>
+            <button>Log In with Metamask</button>
+        </div>
+    )
+}
---
+++ frontend/app/root.tsx
@@ -0,0 +1,46 @@
+import { MetaFunction, LoaderFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import { MoralisProvider } from "react-moralis";
+import { useLoaderData } from "@remix-run/react";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export const loader: LoaderFunction = () => {
+  return {
+    MORALIS_SERVER_URL: process.env.MORALIS_SERVER_URL,
+    MORALIS_APP_ID: process.env.MORALIS_APP_ID,
+  }
+}
+
+export default function App() {
+
+  const loaderData = useLoaderData();
+
+  return (
+    <MoralisProvider serverUrl={`${loaderData.MORALIS_SERVER_URL}`} appId={`${loaderData.MORALIS_APP_ID}`}>
+      <html lang="en">
+        <head>
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    </MoralisProvider>
+  );
+}
